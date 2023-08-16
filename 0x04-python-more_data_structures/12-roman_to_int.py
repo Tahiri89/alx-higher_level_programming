37b14b76c7633f02dc604117d6a203db9103ffce
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-08-16 06:29:57 -0700

    12-roman modify

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    '''function that converts a roman numeral to an integer'''
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
-    roman = {"I": 1, "V": 5, "IX": 9, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    integer = 0
-    for i in range(len(roman_string)):
-        if roman.get(roman_string[i], 0) == 0:
-            return (0)
-        if (i != (len(roman_string) - 1) and
-                roman[roman_string[i]] < roman[roman_string[i + 1]]):
-            integer += roman[roman_string[i]] * -1
-        else:
-            integer += roman[roman_string[i]]    
-    return (integer)
+    sum = 0
+    if(roman_string and isinstance(roman_string, str)):
+        iT = {'D': 500, 'C': 100, 'M': 1000, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        aList = [iT.get(x) for x in roman_string]
+        for idx, char in enumerate(aList):
+            if(idx < len(aList) - 1):
+                if aList[idx + 1] > char:
+                    sum -= char
+                else:
+                    sum += char
+            else:
+                sum += char
+    return(sum)
